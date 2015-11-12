3ac6e49781524b97d1cdade8e2b76ce175800cca
Dolgov Dmitriy <d.dolgov@i2013.ru>
2015-11-12 20:39:39 +0300

    удаление лишнего js кода из view images

--- app/views/images/index.php
+++ app/views/images/index.php
@@ -1,19 +1,11 @@
 <script type="text/javascript">
-
-    function prepareAjaxData(data)
-    {
-        data.ajax = true;
-        return data;
-    }
-
-    console.log("fg");
     $(function() {
         var imageHeight = 200;  // px
 
         var width = $("#content").width();
         var height = $("#content").height();
 
-        var data = prepareAjaxData({"width": width, "height": height});
+        var data = {"width": width, "height": height};
         $.ajax({type: "POST",
             dataType: "json",
             url: "/images/retrieve",
