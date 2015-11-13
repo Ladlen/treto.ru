ce07d3e3f493fb55ddc7fbe0d5f2907d9dbf2e46
Dolgov Dmitriy <d.dolgov@i2013.ru>
2015-11-13 05:29:05 +0300

    начало разработки распределения изображений

--- app/views/images/index.php
+++ app/views/images/index.php
@@ -1,23 +1,116 @@
 <script type="text/javascript">
-    $(function() {
+    $(function () {
         var imageHeight = 200;  // px
+        var imageDistance = 2;  // px - расстояние по краям изображений
 
-        var width = $("#content").width();
-        var height = $("#content").height();
+        var contentWidth = $("#content").width();
+        var contentHeight = $("#content").height();
 
-        var data = {"width": width, "height": height};
-        $.ajax({type: "POST",
+        /**
+         * Вычисление ширины прямоугольника при масштабировании по высоте.
+         * 
+         * @param float imageHeight оригинальная высота
+         * @param float imageWidth оригинальная ширина
+         * @param float newHeight новая высота
+         * @return float вычисленная ширина
+         */
+        function widthByHeight(imageWidth, imageHeight, newHeight) {
+            var newWidth = (newHeight / imageHeight) * imageWidth;
+            return newWidth;
+        }
+
+        /**
+         * Вычисление высоты прямоугольника при масштабировании по ширине.
+         *
+         * @param float imageHeight оригинальная высота
+         * @param float imageWidth оригинальная ширина
+         * @param float newWidth новая ширина
+         * @return float вычисленная высота
+         */
+        function heightByWidth(imageWidth, imageHeight, newWidth) {
+            var newHeight = (newWidth / imageWidth) * imageHeight;
+            return newHeight;
+        }
+
+        function calculateLine(images) {
+            var currentWidth = 0;
+            for (var info in images) {
+                var newWidth = widthByHeight(info.width, info.height, imageHeight);
+                currentWidth += newWidth + imageDistance * 2;
+                if (currentWidth > contentWidth) {
+                    // проверим общую потерю тела изображений
+                    // 1-й вариант: выступающее изображение вписывается в текущую строку
+
+
+                    // 2-й вариант: выступающее изображение переносится на текущую строку
+                }
+            }
+        }
+
+        /**
+         * Вписать изображения images в ширину width, одновременно вычислив общие потери площади при обрезке.
+         *
+         * @param array images список данных изображений
+         * @param int width ширина к которой надо привести
+         */
+        function adjustToLine(images, width) {
+
+            var result = {"newImagesParameters" : [], "totalAreaWaste" : 0};
+
+            var totalImagesWidth = 0;   // ширина изображений в необрезанном виде
+            //for (var info in images) {
+            //    totalImagesWidth += widthByHeight(info.width, info.height, imageHeight);
+            //}
+
+            $.map(images, function(val) {
+                totalImagesWidth += widthByHeight(info.width, info.height, imageHeight);
+            });
+
+            var ratio = totalImagesWidth / width;
+
+            if (ratio > 1) {
+                // обрезка справа/слева
+
+            } else if (ratio < 1) {
+                // обрезка сверху/снизу
+                $.map(images, function(val) {
+                    var width = widthByHeight(info.width, info.height, imageHeight) / ratio;
+                    heightByWidth()
+                    result.newImagesParameters.push({
+                        "width" : width,
+                        "height" : imageHeight,
+                        "left" : left,
+                        "top" : top
+                    });
+                });
+            } else {
+                // ничего не обрезаем
+                $.map(images, function(val) {
+                    var width = widthByHeight(info.width, info.height, imageHeight);
+                    result.newImagesParameters.push({
+                        "width" : width,
+                        "height" : imageHeight,
+                        "left" : left,
+                        "top" : top
+                    });
+                });
+            }
+
+            return result;
+        }
+
+
+        //var data = {"width": width, "height": height};
+        $.ajax({
+            type: "POST",
             dataType: "json",
             url: "/images/retrieve",
-            data: data,
-            success: function(data)
-            {
-                if(data.success)
-                {
-                    $("#list .row[data-id=" + id + "] div." + name).text(value);
+            //data: data,
+            success: function (data) {
+                if (data.success) {
+
                 }
-                else
-                {
+                else {
                     var msg = 'Ошибка запроса:\n' + data.messages.join('\n');
                     alert(msg);
                 }
