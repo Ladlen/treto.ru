c0aa03c3b8bbd833cb27a74005b273f235e49891
Dolgov Dmitriy <d.dolgov@i2013.ru>
2015-11-16 01:12:46 +0300

    добавлен сессионный компонент StringReader

---
+++ app/components/StringReaderSessionComponent.php
@@ -0,0 +1,49 @@
+<?php
+
+class StringReaderSessionComponent
+{
+    protected $config;
+
+    /**
+     * @var string сессионный ключ текущего элемента
+     */
+    protected $elementSessionKey;
+
+    protected $reader;
+
+    public function __construct($config, $elementSessionKey)
+    {
+        $this->config = $config;
+        $this->elementSessionKey = $elementSessionKey;
+
+        session_start();
+        if (!isset($_SESSION[$this->elementSessionKey]))
+        {
+            $_SESSION[$this->elementSessionKey] = [];
+        }
+
+        if (isset($_SESSION[$this->elementSessionKey]['reader']))
+        {
+            $this->reader = unserialize($_SESSION[$this->elementSessionKey]['reader']);
+        }
+        else
+        {
+            $this->reader = new StringReaderComponent($this->config);
+        }
+    }
+
+    public function getNextString()
+    {
+        return $this->reader->getNextString();
+    }
+
+    public function addFile($fileName, $EOL)
+    {
+        return $this->reader->addFile($fileName, $EOL);
+    }
+
+    public function __destruct()
+    {
+        $_SESSION[$this->elementSessionKey]['reader'] = serialize($this->reader);
+    }
+}
